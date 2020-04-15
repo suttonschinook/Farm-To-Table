3bd027aab30de43827186c649eea448ec7f58fe1
Evan <evan.c.sutton1@gmail.com>
2020-04-15 14:04:47 -0600

    Organizing Components

--- farm-to-table/src/App.js
+++ farm-to-table/src/App.js
@@ -5,6 +5,8 @@ export default function App() {
 	return (
 		<div>
 			<Switch>
+				<Route exact path="about" component={About} />
+				<Route exact path="recipes" component={Recipes} />
 				<Route path="/" component={Home} />
 			</Switch>
 		</div>
---
+++ farm-to-table/src/Pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../ReusableComponents/Header'
+import Navbar from '../ReusableComponents/Navbar'
+import Info from '../Content/Info'
+import Footer from '../ReusableComponents/Footer'
+
+export default function About() {
+	return (
+		<div>
+			<Header />
+			<Navbar />
+			<Info />
+			<Footer />
+		</div>
+	)
+}
