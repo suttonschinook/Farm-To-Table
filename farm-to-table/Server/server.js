b66174f1e8be18589098c86eab5ddb72b34e230e
Brandon Artigue <bartigue87@yahoo.com>
2020-04-15 10:25:10 -0600

    initial commit

--- farm-to-table/Server/server.js
+++ farm-to-table/Server/server.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-	res.send('this is home')
-})
+app.get("/", (req, res) => {
+  res.send("this is home");
+});
 
-app.get('/user', (req, res) => {
-	res.send({ name: 'joe', age: 25 })
-})
+app.get("/user", (req, res) => {
+  res.send({ name: "joe", age: 25 });
+});
 
 app.listen(9000, () => {
-	console.log('The server is running')
-})
+  console.log("The server is running");
+});
+
+//Test
