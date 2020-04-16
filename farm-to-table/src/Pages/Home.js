3b4b1eb042cb41363ba2434c3af81885134059ad
Evan Sutton <evan.c.sutton1@gmail.com>
2020-04-16 14:41:35 -0600

    Updated Navbar

--- farm-to-table/src/Pages/Home.js
+++ farm-to-table/src/Pages/Home.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import Header from '../ReusableComponents/Header'
-import Navbar from '../ReusableComponents/Navbar'
-import Main from '../Content/Main'
-import Footer from '../ReusableComponents/Footer'
+import React from "react";
+import Header from "../ReusableComponents/Header";
+import BootstrapNav from "../ReusableComponents/BootstrapNav";
+import Main from "../Content/Main";
+import Footer from "../ReusableComponents/Footer";
 
 export default function Home() {
-	return (
-		<div>
-			<Header />
-			<Navbar />
-			<Main />
-			<Footer />
-		</div>
-	)
+  return (
+    <div>
+      <BootstrapNav />
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
