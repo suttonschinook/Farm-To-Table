358c7005a50b77a3e1820a23308aaedc5119528a
sadie o'neill <sadie.r.oneill@gmail.com>
2020-04-16 19:27:45 -0600

    added carousel and updated navbar and header

--- farm-to-table/src/Content/Main.js
+++ farm-to-table/src/Content/Main.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import Carousel from 'react-bootstrap/Carousel'
 
 export default function Main() {
+
+  function ControlledCarousel() {
+    const [index, setIndex] = useState(0);
+  
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+  
+    return (
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1474440692490-2e83ae13ba29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+            alt="First slide"
+            
+          />
+          <Carousel.Caption>
+            <h1>Sustainable Farms</h1>
+            <h3></h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1495570689269-d883b1224443?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1789&q=80"
+            alt="Second slide"
+          />
+  
+          <Carousel.Caption>
+            <h1>Fresh Produce</h1>
+            <h3></h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1528505086635-4c69d5f10908?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1089&q=80"
+            alt="Third slide"
+          />
+  
+          <Carousel.Caption>
+            <h1>Locally Sourced</h1>
+            <h3>
+              
+            </h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+    
+  }
   return (
-    <>
-      <p>Main Content</p>
-    </>
+    <div>
+      <ControlledCarousel />
+    </div>
   );
 }
+  
+  
--- farm-to-table/src/ReusableComponents/Header.js
+++ farm-to-table/src/ReusableComponents/Header.js
@@ -1,16 +1,27 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
+import styled from "styled-components"
+
+
+const StyledHeader = styled.h1`
+  color:rgb(43, 42, 42);
+  font-family:Georgia, 'Times New Roman', Times, serif;
+  font-size:100px;
+`
+const StyledFarmer = styled.h1`
+color:rgb(43, 42, 42);
+font-family:Georgia, 'Times New Roman', Times, serif;
+font-size:25px;
+
+`
 
 export default function Header() {
   return (
     <Jumbotron fluid>
       <Container>
-        <h1>Fluid jumbotron</h1>
-        <p>
-          This is a modified jumbotron that occupies the entire horizontal space
-          of its parent.
-        </p>
+        <StyledHeader>Farm to Table</StyledHeader>
+        <StyledFarmer>A virtual farmers market experience</StyledFarmer>
       </Container>
     </Jumbotron>
   );
