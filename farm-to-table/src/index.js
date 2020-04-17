724a811a007b78637faf30e57249128bf8a630bd
Evan Sutton <evan.c.sutton1@gmail.com>
2020-04-17 14:22:41 -0600

    Added ProduceContext to index

--- farm-to-table/src/index.js
+++ farm-to-table/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.js'
-import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import ProduceContext from "./Context/ProduceContext";
+import App from "./App.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById('root')
-)
+  <BrowserRouter>
+    <ProduceContext>
+      <App />
+    </ProduceContext>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 //Take a rook at me now. Take a rook at me noooooooowww
