47542bb39526ef2ad3cb79968428d492e8c455db
sadie o'neill <sadie.r.oneill@gmail.com>
2020-04-21 11:21:15 -0600

    header

--- farm-to-table/src/ReusableComponents/Header.js
+++ farm-to-table/src/ReusableComponents/Header.js
@@ -9,8 +9,8 @@ const StyledHeader = styled.h1`
   display:flex;
   justify-content:center;
   color:rgb(43, 42, 42);
-  font-family:Georgia, 'Times New Roman', Times, serif;
-  font-size:80px;
+  font-family:'Roboto', sans-serif;
+  font-size:90px;
   text-shadow: 2px 2px darkgray;
   height:5vh;
   margin-top:-3%;
@@ -19,7 +19,7 @@ const StyledFarmer = styled.h2`
 display:flex;
 justify-content:center;
 color:rgb(43, 42, 42);
-font-family:Georgia, 'Times New Roman', Times, serif;
+font-family:'Roboto', sans-serif;
 font-size:20px;
 font-style:italic;
 height:3px;
@@ -31,7 +31,7 @@ export default function Header() {
     
     <Jumbotron fluid>
       <Container>
-        <StyledHeader>Farm to Table</StyledHeader>
+        <StyledHeader className = "header">Farm to Table</StyledHeader>
         <StyledFarmer>A virtual farmers market experience</StyledFarmer>
       </Container>
     </Jumbotron>
