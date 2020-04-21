f6f02a970ae96ccc7b5c9074b1586c6e5bd175f3
Evan Sutton <evan.c.sutton1@gmail.com>
2020-04-21 12:20:59 -0600

    Header Template

--- farm-to-table/src/ReusableComponents/Header.js
+++ farm-to-table/src/ReusableComponents/Header.js
@@ -1,40 +1,36 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
-import styled from "styled-components"
-
-
+import styled from "styled-components";
 
 const StyledHeader = styled.h1`
-  display:flex;
-  justify-content:center;
-  color:rgb(43, 42, 42);
-  font-family:Georgia, 'Times New Roman', Times, serif;
-  font-size:80px;
+  display: flex;
+  justify-content: center;
+  color: rgb(43, 42, 42);
+  font-family: Georgia, "Times New Roman", Times, serif;
+  font-size: 80px;
   text-shadow: 2px 2px darkgray;
-  height:5vh;
-  margin-top:-3%;
-`
+  height: 5vh;
+  margin-top: -3%;
+`;
 const StyledFarmer = styled.h2`
-display:flex;
-justify-content:center;
-color:rgb(43, 42, 42);
-font-family:Georgia, 'Times New Roman', Times, serif;
-font-size:20px;
-font-style:italic;
-height:3px;
-margin-top:6%;
-`
+  display: flex;
+  justify-content: center;
+  color: rgb(43, 42, 42);
+  font-family: Georgia, "Times New Roman", Times, serif;
+  font-size: 20px;
+  font-style: italic;
+  height: 3px;
+  margin-top: 6%;
+`;
 
 export default function Header() {
   return (
-    
     <Jumbotron fluid>
       <Container>
         <StyledHeader>Farm to Table</StyledHeader>
         <StyledFarmer>A virtual farmers market experience</StyledFarmer>
       </Container>
     </Jumbotron>
-    
   );
 }
