bfe0b73bc03f440ae737865d8b9c43e223558ddd
Evan Sutton <evan.c.sutton1@gmail.com>
2020-04-23 13:49:51 -0600

    DisplayProduce.js

--- farm-to-table/src/Pages/Produce.js
+++ farm-to-table/src/Pages/Produce.js
@@ -3,6 +3,15 @@ import Header from "../ReusableComponents/Header";
 import BootstrapNav from "../ReusableComponents/BootstrapNav";
 import Footer from "../ReusableComponents/Footer";
 import BuyProduce from "../Content/BuyProduce";
+import styled from "styled-components";
+
+const StyledProduce = styled.div`
+  display: flex;
+  width: 50vw;
+  margin: auto;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function Home() {
   return (
