10b60c836fb9507910339c1089c7dd58b25cf1a4
Evan Sutton <evan.c.sutton1@gmail.com>
2020-04-24 11:34:42 -0600

    ProduceContext Fix

--- farm-to-table/src/Context/ProduceContext.js
+++ farm-to-table/src/Context/ProduceContext.js
@@ -4,29 +4,7 @@ const axios = require("axios");
 
 export default function ProduceProvider(props) {
   const [produceState, setProduceState] = useState({
-    produceAvailable: [
-      {
-        item: "Apples",
-        price: 3,
-        description:
-          "Who doesn't like fresh apples? If someone doesn't like apples they should not be trusted......",
-        img: "http://www.pngall.com/wp-content/uploads/2016/04/Apple-Fruit.png",
-      },
-      // {
-      //   item: "Apples",
-      //   price: 3,
-      //   description:
-      //     "Who doesn't like fresh apples? If someone doesn't like apples they should not be trusted......",
-      //   img: "http://www.pngall.com/wp-content/uploads/2016/04/Apple-Fruit.png",
-      // },
-      // {
-      //   item: "Apples",
-      //   price: 3,
-      //   description:
-      //     "Who doesn't like fresh apples? If someone doesn't like apples they should not be trusted......",
-      //   img: "http://www.pngall.com/wp-content/uploads/2016/04/Apple-Fruit.png",
-      // },
-    ],
+    produceAvailable: [],
   });
   function getProduce() {
     axios.get("/produce").then((response) => {
