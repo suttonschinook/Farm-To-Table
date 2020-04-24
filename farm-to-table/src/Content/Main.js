4fd19c1125a070ba6cb97dae125ac318494cebf0
Evan Sutton <evan.c.sutton1@gmail.com>
2020-04-24 13:24:20 -0600

    Fixed Navbar Icons and Routing

--- farm-to-table/src/Content/Main.js
+++ farm-to-table/src/Content/Main.js
@@ -1,34 +1,31 @@
 import React, { useState } from "react";
-import Carousel from 'react-bootstrap/Carousel'
-import styled from "styled-components"
+import Carousel from "react-bootstrap/Carousel";
+import styled from "styled-components";
 
 const Container = styled.div`
-
-  width:750px;
-  margin-left:22%;
-`
+  width: 750px;
+  margin-left: 22%;
+`;
 
 export default function Main() {
-
   function ControlledCarousel() {
     const [index, setIndex] = useState(0);
-  
+
     const handleSelect = (selectedIndex, e) => {
       setIndex(selectedIndex);
     };
-  
+
     return (
       <Container>
         <Carousel activeIndex={index} onSelect={handleSelect}>
-          <Carousel.Item className = "slide1">
+          <Carousel.Item className="slide1">
             <img
               className="d-block w-100"
               src="https://images.unsplash.com/photo-1474440692490-2e83ae13ba29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
               alt="First slide"
             />
             <Carousel.Caption>
-                <h1>Sustainable Farms</h1>
-                <h3></h3>
+              <h1>Sustainable Farms</h1>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
@@ -39,7 +36,6 @@ export default function Main() {
             />
             <Carousel.Caption>
               <h1>Fresh Produce</h1>
-              <h3></h3>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
@@ -53,9 +49,8 @@ export default function Main() {
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
-      </Container> 
-      
-  );
+      </Container>
+    );
   }
   return (
     <div>
@@ -63,13 +58,3 @@ export default function Main() {
     </div>
   );
 }
-
-            
-           
-            
-  
-    
-     
-     
-  
-  
