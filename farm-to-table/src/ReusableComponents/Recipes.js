2b9dc6dffe64f0502708c58ef1ba60d2dd37fa32
Evan Sutton <evan.c.sutton1@gmail.com>
2020-04-29 10:05:39 -0600

    recipe restyling

--- farm-to-table/src/ReusableComponents/Recipes.js
+++ farm-to-table/src/ReusableComponents/Recipes.js
@@ -1,18 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Card from 'react-bootstrap/Card';
+import styled from 'styled-components';
+
+const StyledRecipe = styled.div`
+	display: flex;
+	width: 50vw;
+	margin: auto;
+	justify-content: center;
+	align-items: center;
+`;
 
 export default function Recipes(props) {
-  const { title, image, id } = props;
-  return (
-    <div>
-      <div className="meal">
-        <Link to={`/${id}`}>
-          <img className="mealImg" src={image} alt="" />
-          <div className="meal-info">
-            <h1>{title}</h1>
-          </div>
-        </Link>
-      </div>
-    </div>
-  );
+	const { title, image, id } = props;
+	return (
+		<>
+			<StyledRecipe className='meal'>
+				<CardDeck>
+					<Link to={`/${id}`}>
+						<Card style={{ width: '240px' }}>
+							<Card.Img
+								variant='top'
+								height='160px'
+								src={image}
+								alt=''
+								className='mealImg'
+							/>
+							<Card.Body>
+								<Card.Title>{title}</Card.Title>
+							</Card.Body>
+						</Card>
+					</Link>
+				</CardDeck>
+			</StyledRecipe>
+		</>
+	);
 }
